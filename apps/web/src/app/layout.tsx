a0acab919c1a11cef7e26526882b9bd56f965db3
Celo Composer <support@celo.org>
2026-05-05 07:31:02 +0100

    feat: initial commit from Celo Composer

---
+++ apps/web/src/app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+
+import { Navbar } from '@/components/navbar';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'swiftlink',
+  description: 'A new Celo blockchain project',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        {/* Navbar is included on all pages */}
+        <div className="relative flex min-h-screen flex-col">
+          <Navbar />
+          <main className="flex-1">
+            {children}
+          </main>
+                  </div>
+      </body>
+    </html>
+  );
+}
