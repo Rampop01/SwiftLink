95ca377ae550eb2b17e71d27d6792b27f6d7e42b
rampop01 <popoolarahmat96@gmail.com>
2026-06-18 21:12:56 +0100

    feat: add array max finder

--- apps/web/src/lib/helpers.ts
+++ apps/web/src/lib/helpers.ts
@@ -62,3 +62,5 @@ export const isEmpty = (arr: any[]) => arr.length === 0;
 export const sum = (arr: number[]) => arr.reduce((a, b) => a + b, 0);
 
 export const min = (arr: number[]) => Math.min(...arr);
+
+export const max = (arr: number[]) => Math.max(...arr);
