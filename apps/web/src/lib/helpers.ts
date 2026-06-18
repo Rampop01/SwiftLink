f8c52689fcbcaf20fd0bf0987dba0da2f511eb32
rampop01 <popoolarahmat96@gmail.com>
2026-06-18 21:12:56 +0100

    feat: add math abs alias

--- apps/web/src/lib/helpers.ts
+++ apps/web/src/lib/helpers.ts
@@ -108,3 +108,5 @@ export const round = (n: number) => Math.round(n);
 export const ceil = (n: number) => Math.ceil(n);
 
 export const floor = (n: number) => Math.floor(n);
+
+export const abs = (n: number) => Math.abs(n);
