b6153d9fa8c5062c142d9ac0f8af271e767a1c84
rampop01 <popoolarahmat96@gmail.com>
2026-06-18 21:12:56 +0100

    feat: add array sum calculator

--- apps/web/src/lib/helpers.ts
+++ apps/web/src/lib/helpers.ts
@@ -58,3 +58,5 @@ export const first = <T>(arr: T[]) => arr[0];
 export const last = <T>(arr: T[]) => arr[arr.length - 1];
 
 export const isEmpty = (arr: any[]) => arr.length === 0;
+
+export const sum = (arr: number[]) => arr.reduce((a, b) => a + b, 0);
