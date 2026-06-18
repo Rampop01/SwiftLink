c05ae5f70b7c8252ee82a48c271d7a23ee91f5cd
rampop01 <popoolarahmat96@gmail.com>
2026-06-18 21:12:55 +0100

    feat: add isObject helper

--- apps/web/src/lib/helpers.ts
+++ apps/web/src/lib/helpers.ts
@@ -2,3 +2,5 @@
 export const isString = (val: any): val is string => typeof val === 'string';
 
 export const isNumber = (val: any): val is number => typeof val === 'number';
+
+export const isObject = (val: any) => val !== null && typeof val === 'object';
