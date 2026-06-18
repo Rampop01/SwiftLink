ae93ab6ebb39b9d5e59660fb9102900963a8d389
rampop01 <popoolarahmat96@gmail.com>
2026-06-18 21:12:55 +0100

    feat: add url slugify formatter

--- apps/web/src/lib/helpers.ts
+++ apps/web/src/lib/helpers.ts
@@ -38,3 +38,5 @@ export const fromHex = (hex: string) => parseInt(hex, 16);
 export const getTimestamp = () => Date.now();
 
 export const isValidEmail = (e: string) => /^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$/.test(e);
+
+export const slugify = (s: string) => s.toLowerCase().replace(/[^a-z0-9]+/g, '-');
