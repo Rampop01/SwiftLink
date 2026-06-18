002c225e69a0a177f29e712812a54520647dd624
rampop01 <popoolarahmat96@gmail.com>
2026-06-18 21:12:55 +0100

    feat: add string capitalization util

--- apps/web/src/lib/helpers.ts
+++ apps/web/src/lib/helpers.ts
@@ -4,3 +4,5 @@ export const isString = (val: any): val is string => typeof val === 'string';
 export const isNumber = (val: any): val is number => typeof val === 'number';
 
 export const isObject = (val: any) => val !== null && typeof val === 'object';
+
+export const capitalize = (s: string) => s.charAt(0).toUpperCase() + s.slice(1);
