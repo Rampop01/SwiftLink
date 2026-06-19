b3ce1b61583c8039927f14e6c5ed3e84b3d6f127
rampop01 <popoolarahmat96@gmail.com>
2026-06-19 22:07:08 +0100

    feat: add milesToKm converter

--- apps/web/src/lib/validators.ts
+++ apps/web/src/lib/validators.ts
@@ -146,3 +146,5 @@ export const kgToLbs = (kg: number) => kg * 2.20462;
 export const lbsToKg = (lbs: number) => lbs / 2.20462;
 
 export const kmToMiles = (km: number) => km * 0.621371;
+
+export const milesToKm = (miles: number) => miles / 0.621371;
