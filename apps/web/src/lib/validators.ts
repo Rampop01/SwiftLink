9637cde3c1f7938ab6b2f7955691a95d67c75890
rampop01 <popoolarahmat96@gmail.com>
2026-06-19 22:07:06 +0100

    feat: add isJSON safe validator

--- apps/web/src/lib/validators.ts
+++ apps/web/src/lib/validators.ts
@@ -8,3 +8,5 @@ export const isAlphanumeric = (s: string) => /^[a-zA-Z0-9]+$/.test(s);
 export const isHexColor = (s: string) => /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/.test(s);
 
 export const isUUID = (s: string) => /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(s);
+
+export const isJSON = (s: string) => { try { JSON.parse(s); return true; } catch { return false; } };
