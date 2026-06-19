5a87fa42d4c4115c729b1fb847df541967a6fc98
rampop01 <popoolarahmat96@gmail.com>
2026-06-19 22:07:06 +0100

    feat: add isHexColor string validator

--- apps/web/src/lib/validators.ts
+++ apps/web/src/lib/validators.ts
@@ -4,3 +4,5 @@ export const isAlpha = (s: string) => /^[a-zA-Z]+$/.test(s);
 export const isNumeric = (s: string) => /^[0-9]+$/.test(s);
 
 export const isAlphanumeric = (s: string) => /^[a-zA-Z0-9]+$/.test(s);
+
+export const isHexColor = (s: string) => /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/.test(s);
