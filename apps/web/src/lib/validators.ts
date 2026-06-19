4101a36641a8fd4b2fe4b1d8dc1b3c3cd7c012e1
rampop01 <popoolarahmat96@gmail.com>
2026-06-19 22:07:06 +0100

    feat: add isUUID string validator

--- apps/web/src/lib/validators.ts
+++ apps/web/src/lib/validators.ts
@@ -6,3 +6,5 @@ export const isNumeric = (s: string) => /^[0-9]+$/.test(s);
 export const isAlphanumeric = (s: string) => /^[a-zA-Z0-9]+$/.test(s);
 
 export const isHexColor = (s: string) => /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/.test(s);
+
+export const isUUID = (s: string) => /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(s);
