8573a09a04c62d187ee9e1e316d6454445e0d3a0
rampop01 <popoolarahmat96@gmail.com>
2026-06-19 22:07:07 +0100

    feat: add throttle wrapper

--- apps/web/src/lib/validators.ts
+++ apps/web/src/lib/validators.ts
@@ -80,3 +80,5 @@ export const omit = (obj: any, keys: string[]) => Object.keys(obj).filter(k => !
 export const pick = (obj: any, keys: string[]) => keys.reduce((res: any, k) => { if (k in obj) res[k] = obj[k]; return res; }, {});
 
 export const debounce = (fn: Function, ms = 300) => { let t: any; return function(this: any, ...args: any[]) { clearTimeout(t); t = setTimeout(() => fn.apply(this, args), ms); }; };
+
+export const throttle = (fn: Function, wait = 300) => { let inThrottle: boolean; return function(this: any, ...args: any[]) { if (!inThrottle) { fn.apply(this, args); inThrottle = true; setTimeout(() => inThrottle = false, wait); } }; };
