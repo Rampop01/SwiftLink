3e406f750faca9afde00b1f25fa432a13af2c466
rampop01 <popoolarahmat96@gmail.com>
2026-06-20 23:59:07 +0100

    feat: add area of rectangle formula

--- apps/web/src/lib/math-utils.ts
+++ apps/web/src/lib/math-utils.ts
@@ -2,3 +2,5 @@
 export const areaOfCircle = (r: number) => Math.PI * r * r;
 
 export const circumferenceOfCircle = (r: number) => 2 * Math.PI * r;
+
+export const areaOfRectangle = (w: number, h: number) => w * h;
