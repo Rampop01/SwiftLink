ef778428fbf8f88cdd91775729064c152eecebdc
rampop01 <popoolarahmat96@gmail.com>
2026-06-20 23:59:08 +0100

    feat: add celsius to kelvin conversion

--- apps/web/src/lib/math-utils.ts
+++ apps/web/src/lib/math-utils.ts
@@ -122,3 +122,5 @@ export const energyPhoton = (frequency: number) => 6.62607015e-34 * frequency;
 export const massEnergyEquivalence = (mass: number) => mass * Math.pow(299792458, 2);
 
 export const idealGasPressure = (n: number, v: number, t: number) => (n * 8.314 * t) / v;
+
+export const celsiusToKelvin = (c: number) => c + 273.15;
