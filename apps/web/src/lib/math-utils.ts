3f0d5305f27c34be32029a84c266687b8eb042f6
rampop01 <popoolarahmat96@gmail.com>
2026-06-20 23:59:07 +0100

    feat: add recursive factorial function

--- apps/web/src/lib/math-utils.ts
+++ apps/web/src/lib/math-utils.ts
@@ -24,3 +24,5 @@ export const volumeOfSphere = (r: number) => (4/3) * Math.PI * Math.pow(r, 3);
 export const surfaceAreaOfSphere = (r: number) => 4 * Math.PI * r * r;
 
 export const volumeOfCone = (r: number, h: number) => (1/3) * Math.PI * r * r * h;
+
+export const factorial = (n: number): number => n <= 1 ? 1 : n * factorial(n - 1);
