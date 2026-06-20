3da286040b9df15c2adc9af1d4b16c831e41a4e0
rampop01 <popoolarahmat96@gmail.com>
2026-06-20 23:59:07 +0100

    feat: add area of triangle formula

--- apps/web/src/lib/math-utils.ts
+++ apps/web/src/lib/math-utils.ts
@@ -6,3 +6,5 @@ export const circumferenceOfCircle = (r: number) => 2 * Math.PI * r;
 export const areaOfRectangle = (w: number, h: number) => w * h;
 
 export const perimeterOfRectangle = (w: number, h: number) => 2 * (w + h);
+
+export const areaOfTriangle = (b: number, h: number) => 0.5 * b * h;
